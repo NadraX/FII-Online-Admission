1e6539281737e0094cc1fcc4888cd6f355722f05
Irinel Manolache <Irinel Manolache>
2017-05-24 23:52:21 +0300

    Baza de date functionala

---
+++ Baza de date/PopulateDB.sql
@@ -0,0 +1,126 @@
+INSERT INTO date_medie_concurs values(1,9.40,9.10,9.90,8.50);
+INSERT INTO date_medie_concurs values(2,9.40,9.35,8.95,9.60);
+INSERT INTO date_medie_concurs values(3,8.40,9.10,7.95,7.80);
+INSERT INTO date_medie_concurs values(4,7.40,8.10,8.95,9.60);
+INSERT INTO date_medie_concurs values(5,6.40,7.10,9.70,8.80);
+INSERT INTO date_medie_concurs values(6,8.40,9.50,9.50,8.90);
+INSERT INTO date_medie_concurs values(7,7.40,7.30,8.95,9.50);
+INSERT INTO date_medie_concurs values(8,9.40,8.10,9.50,8.50);
+INSERT INTO date_medie_concurs values(9,6.40,7.90,9.10,7.50);
+INSERT INTO date_medie_concurs values(10,8.40,9.90,9.95,9.50);
+
+INSERT INTO OLIMPIADA VALUES(1,'1','INFORMATICA',2017,1);
+INSERT INTO OLIMPIADA VALUES(2,'1','INFORMATICA',2016,10);
+INSERT INTO OLIMPIADA VALUES(3,'1','INFORMATICA',2015,7);
+INSERT INTO OLIMPIADA VALUES(4,'1','INFORMATICA',2017,8);
+INSERT INTO OLIMPIADA VALUES(5,'1','INFORMATICA',2015,20);
+INSERT INTO OLIMPIADA VALUES(6,'1','INFORMATICA',2014,21);
+INSERT INTO OLIMPIADA VALUES(7,'1','INFORMATICA',2017,31);
+INSERT INTO OLIMPIADA VALUES(8,'1','INFORMATICA',2016,69);
+INSERT INTO OLIMPIADA VALUES(9,'1','INFORMATICA',2017,4);
+INSERT INTO OLIMPIADA VALUES(10,'1','INFORMATICA',2017,5);
+
+
+INSERT INTO OLIMPIC VALUES(1,1);
+INSERT INTO OLIMPIC VALUES(2,2);
+INSERT INTO OLIMPIC VALUES(3,3);
+INSERT INTO OLIMPIC VALUES(4,4);
+INSERT INTO OLIMPIC VALUES(5,5);
+INSERT INTO OLIMPIC VALUES(1,6);
+INSERT INTO OLIMPIC VALUES(7,7);
+INSERT INTO OLIMPIC VALUES(8,8);
+INSERT INTO OLIMPIC VALUES(9,9);
+INSERT INTO OLIMPIC VALUES(10,10);
+
+---XXXXXXXXXXXXXXXXXXXXX
+Insert into DETALII_APLICARE values(1,'licenta','Informatica','Romana','1','Programare Competitiva','1');
+Insert into DETALII_APLICARE values(2,'preadmitere','Informatica','Engleza','0','Programare Competitiva','1');
+Insert into DETALII_APLICARE values(3,'master','Informatica','Romana','1','Programare Competitiva','1');
+Insert into DETALII_APLICARE values(4,'master','Informatica','Engleza','0','Programare Competitiva','1');
+Insert into DETALII_APLICARE values(5,'master','Informatica','Romana','1','Programare Competitiva','1');
+Insert into DETALII_APLICARE values(6,'preadmitere','Informatica','Engleza','0','Programare Competitiva','1');
+Insert into DETALII_APLICARE values(7,'licenta','Informatica','Romana','1','Programare Competitiva','1');
+Insert into DETALII_APLICARE values(8,'licenta','Informatica','Engleza','0','Programare Competitiva','1');
+Insert into DETALII_APLICARE values(9,'licenta','Informatica','Romana','0','Programare Competitiva','1');
+Insert into DETALII_APLICARE values(10,'licenta','Informatica','Engleza','1','Programare Competitiva','1');
+
+
+Insert into DATE_PERSONALE_CANDIDAT values(1,'1960303071375','Atodiresei','Alin','M','M','necasatorit','romana','Viorica','Mihai',
+                                          '0756636643','alin.atod@gmail.com', to_Date('03/03/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP RESITA',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Vaslui','Vaslui','St. Traian',1,'bl.3','1','8',715200);
+Insert into DATE_PERSONALE_CANDIDAT values(2,'1961220071375','Manolache','irinel','M','M','necasatorit','romana','Liliana','Mihai',
+                                          '0756636643','manolache.irinel@gmail.com', to_Date('20/12/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP Bucuresti',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Dorohoi','Dorohoi','St. Independentei',1,'bl.5','2','16',715201);
+Insert into DATE_PERSONALE_CANDIDAT values(3,'1960304071375','Marcu','Alin','M','M','necasatorit','romana','Viorica','Mihai',
+                                          '0756636643','alin.marcu@gmail.com', to_Date('03/03/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP Iasi',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Iasi','Iasi','St. Lt.Stoicescu',1,'bl.6','3','18',715202);
+Insert into DATE_PERSONALE_CANDIDAT values(4,'1960403071375','Radu','Alin','M','M','necasatorit','romana','Viorica','Mihai',
+                                          '0756636643','alin.rd@gmail.com', to_Date('03/04/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP Vaslui',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Vaslui','Vaslui','Strada St. cel Mare',1,'bl.8','4','28',715202);
+Insert into DATE_PERSONALE_CANDIDAT values(5,'1960503071375','Verciuc','Sergiu','M','M','necasatorit','romana','Viorica','Mihai',
+                                          '0756636643','sergiu.ver@gmail.com', to_Date('03/05/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP RESITA',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Iasi','Iasi','Strada St. cel Mare',1,'bl.1','5','12',715203);
+Insert into DATE_PERSONALE_CANDIDAT values(6,'1960603071375','Like','Alin','M','M','necasatorit','romana','Viorica','Mihai',
+                                          '0756636643','alin.like@gmail.com', to_Date('03/06/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP RESITA',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Bucuresti','Bucuresti','St. Independentei',1,'bl.2','6','13',715204);
+Insert into DATE_PERSONALE_CANDIDAT values(7,'1960703071375','Rotaru','Alin','M','M','necasatorit','romana','Viorica','Mihai',
+                                          '0756636643','alin.rot@gmail.com', to_Date('03/07/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP RESITA',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Vaslui','Vaslui','St. Traian',1,'bl.10','7','14',715205);
+Insert into DATE_PERSONALE_CANDIDAT values(8,'1960803071375','Gerasim','Cosmin','M','M','necasatorit','romana','Viorica','Mihai',
+                                          '0756636643','cosmin.ger@gmail.com', to_Date('03/08/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP RESITA',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Vaslui','Vaslui','St. Lt.Stoicescu',1,'bl.7','6','16',715202);
+Insert into DATE_PERSONALE_CANDIDAT values(9,'1960903071375','Grama','Dragos','M','M','necasatorit','romana','Viorica','Mihai',
+                                          '0756636643','dragos.gr@gmail.com', to_Date('03/09/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP RESITA',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Galati','Galati','Strada St. cel Mare',1,'bl.4','4','18',715201);
+Insert into DATE_PERSONALE_CANDIDAT values(10,'1960303071375','Alex','Lupu','M','M','necasatorit','romana','Viorica','Mihai',
+                                          '0756636643','alex.lupu@gmail.com', to_Date('03/03/1996','dd/mm/yyyy'),'romana','romana','Romana',
+                                          'CI','XT','692582','SPCLEP RESITA',to_date('03/04/2015','dd/mm/yyyy'),'Romania','Vaslui','Vaslui','St. Independentei',1,'bl.3','2','20',715202);
+
+
+                           
+insert into institutie values(1,'liceu',to_date('15/08/2012','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'teoretic','real','mate-info',2017,1,'Romania','Vaslui','Vaslui');
+insert into institutie values(2,'liceu',to_date('15/08/2012','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'teoretic','real','mate-info',2017,2,'Romania','Vaslui','Vaslui');
+insert into institutie values(3,'liceu',to_date('15/08/2012','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'teoretic','real','mate-info',2017,3,'Romania','Vaslui','Vaslui');
+insert into institutie values(4,'liceu',to_date('15/08/2012','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'teoretic','real','mate-info',2017,4,'Romania','Vaslui','Vaslui');
+insert into institutie values(5,'liceu',to_date('15/08/2012','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'teoretic','real','mate-info',2017,5,'Romania','Vaslui','Barlad');
+
+insert into PREGATIRI_ANTERIOARE values(1,1);
+insert into PREGATIRI_ANTERIOARE values(2,2);
+insert into PREGATIRI_ANTERIOARE values(3,3);
+insert into PREGATIRI_ANTERIOARE values(4,4);
+insert into PREGATIRI_ANTERIOARE values(5,5); 
+
+Insert into formular values(1,1,1,1,to_date('15/07/2016','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'1','15/07/2016',
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'));
+Insert into formular values(2,2,2,2,to_date('15/07/2016','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'1','15/07/2016',
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'));
+Insert into formular values(3,3,3,3,to_date('15/07/2016','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'1','15/07/2016',
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'));
+Insert into formular values(4,4,4,4,to_date('15/07/2016','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'1','15/07/2016',
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'));
+Insert into formular values(5,5,5,5,to_date('15/07/2016','dd/mm/yyyy'),to_date('15/07/2016','dd/mm/yyyy'),'1','15/07/2016',
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'),
+                            TO_TIMESTAMP ('15-Aug-02 14:10:10.123000', 'DD-Mon-RR HH24:MI:SS.FF'));
+
+
+Insert into Useri values(1, 'alin.atodiresei', 'alabala', 'alin.atod@gmail.com', 'Atodiresei', 'Alin');
+Insert into Useri values(2, 'alin.gerici', 'gerinovici', 'cosmin.ger@gmail.com', 'GEEER', 'TotAlin');
+
+Insert into categorie values (1,'Inregistrare online',null);
+Insert into categorie values (2,'Locatie',null);
+Insert into categorie values (3,'Examen',null);
+Insert into categorie values (4,'Subiecte',3);
+
+Insert into intrebare values (1,'Este posibil sa o fac pe telefon?',1,1);
+Insert into intrebare values (2,'Locatia va avea aer conditionat?',2,2);
+Insert into intrebare values (6,'Cum ajung in 5 minute?',2,2);
+Insert into intrebare values (4,'Unde pun intrebari despre examen?',3,1);
+Insert into intrebare values (5,'Leak de subiecte?',4,1);
+
+commit;
+
+
