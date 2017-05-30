9af64fdfcd0305fbf4cfad2d6bd5b099fde7da3e
Unknown <adrian.dorneanu96@gmail.com>
2017-05-31 00:07:28 +0300

    Adaugat alter table-uri

--- Baza de date/creareV2.1.sql
+++ Baza de date/creareV2.1.sql
@@ -264,3 +264,26 @@ alter table formular
   modify (data_ultimei_modificari date null, data_compozitiei date null, validat varchar2(1 byte) null,
   persoana_ultima_modificare varchar2(50 byte) null, last_update_date timestamp(6) null, 
   serie_chitanta varchar2(20 byte) null, suma_achitata number(6, 2) null);
+
+alter table date_personale_candidat
+    modify DOMICILIU_STABIL varchar2(6 byte)  null;
+    
+alter table detalii_aplicare
+    modify preferinta_limba_studiu varchar2(20 byte) null;
+    
+alter table detalii_aplicare
+    modify stare_sociala_speciala CHAR(1 BYTE) null;
+    
+alter table detalii_aplicare
+    modify nume_stare_sociala VARCHAR2(30 BYTE) null;
+    
+alter table detalii_aplicare
+    modify echivalare_diploma_olimpiada CHAR(1 BYTE) null;
+
+alter table detalii_aplicare
+    modify cazare_pe_timpul_studiilor CHAR(1 BYTE) null;
+    
+alter table detalii_aplicare
+    modify cazare_pe_timpul_examenului CHAR(1 BYTE) null;
+
+
