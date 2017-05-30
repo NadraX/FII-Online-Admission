9306c800085ebace3b7428e4ce2236dd5bc8dfae
Unknown <adrian.dorneanu96@gmail.com>
2017-05-31 00:54:36 +0300

    Update 1 baza de date

--- Baza de date/creareV2.1.sql
+++ Baza de date/creareV2.1.sql
@@ -265,25 +265,6 @@ alter table formular
   persoana_ultima_modificare varchar2(50 byte) null, last_update_date timestamp(6) null, 
   serie_chitanta varchar2(20 byte) null, suma_achitata number(6, 2) null);
 
-alter table date_personale_candidat
-    modify DOMICILIU_STABIL varchar2(6 byte)  null;
-    
-alter table detalii_aplicare
-    modify preferinta_limba_studiu varchar2(20 byte) null;
-    
-alter table detalii_aplicare
-    modify stare_sociala_speciala CHAR(1 BYTE) null;
-    
-alter table detalii_aplicare
-    modify nume_stare_sociala VARCHAR2(30 BYTE) null;
-    
-alter table detalii_aplicare
-    modify echivalare_diploma_olimpiada CHAR(1 BYTE) null;
 
-alter table detalii_aplicare
-    modify cazare_pe_timpul_studiilor CHAR(1 BYTE) null;
-    
-alter table detalii_aplicare
-    modify cazare_pe_timpul_examenului CHAR(1 BYTE) null;
 
 
